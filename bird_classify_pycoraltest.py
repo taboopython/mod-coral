a85a504ff7e23202395940f734b06c5f4ea1f33c
saboten <hellofromeizo@gmail.com>
2021-09-24 13:45:10 +0900

    delete src_size,appsink_size

--- bird_classify_pycoraltest.py
+++ bird_classify_pycoraltest.py
@@ -175,9 +175,9 @@ def main():
 
         last_results=results
         last_time = end_time
-    result = gstreamer.run_pipeline(user_callback,
-                                    src_size=(640, 480),
-                                    appsink_size=inference_size)
+    result = gstreamer.run_pipeline(user_callback)
+                                    #src_size=(640, 480),
+                                    #appsink_size=inference_size)
                                     # delete videosrc=args.videosrc,  videofmt=args.videofmt,headless=args.headless
 
 if __name__ == '__main__':
